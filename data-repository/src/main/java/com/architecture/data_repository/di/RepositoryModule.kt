6e02bc91e38eabba4b2fd95540a3cf97b317a5a2
Borja <borja@i2b.studio>
2023-04-24 16:41:24 +0200

    style: Update marketRepositoryImpl argument naming

--- data-repository/src/main/java/com/architecture/data_repository/di/RepositoryModule.kt
+++ data-repository/src/main/java/com/architecture/data_repository/di/RepositoryModule.kt
@@ -14,6 +14,6 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
-    abstract fun bindMarketRepository(marketRepository: MarketRepositoryImpl): MarketRepository
+    abstract fun bindMarketRepository(marketRepositoryImpl: MarketRepositoryImpl): MarketRepository
 
 }
